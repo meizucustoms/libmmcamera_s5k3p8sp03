5851c5cc2616e4a5a0a291ea1a7a70e0be85d8a0
Roman Rihter <teledurak@gmail.com>
2021-10-10 21:42:03 +0300

    reorganize tree

--- sensors/imx362/Android.mk
+++ sensors/imx362/Android.mk
@@ -2,8 +2,8 @@ IMX362_SENSOR_LIBS_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_PATH             := $(IMX362_SENSOR_LIBS_PATH)
-LOCAL_C_INCLUDES       := $(LOCAL_PATH)/../includes/
-LOCAL_C_INCLUDES       += $(LOCAL_PATH)/../includes/0309
+LOCAL_C_INCLUDES       := $(LOCAL_PATH)/../../includes/
+LOCAL_C_INCLUDES       += $(LOCAL_PATH)/../../includes/0309
 
 LOCAL_C_INCLUDES       += imx362_lib.h
 LOCAL_SRC_FILES        := imx362_lib.c
